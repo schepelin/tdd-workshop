c7babfc73b5ae451180c9c535d3357f3476fde6a
Maxim Schepelin <m.schepelin@gmail.com>
2017-12-06 19:05:34 +0300

    Make after solution faster but harder to reader

--- solution_after.py
+++ solution_after.py
@@ -1,3 +1,4 @@
+import math
 
 
 def is_perfect(number):
@@ -6,9 +7,14 @@ def is_perfect(number):
     factors.append(number)
 
     i = 2
-    while i < number:
+    while i <= int(math.sqrt(number)):
         if number % i == 0:
             factors.append(i)
+            symmetrical_factor = int(number / i)
+
+            # guard condition for numbers e.g. 16
+            if symmetrical_factor != i:
+                factors.append(symmetrical_factor)
         i += 1
 
     return sum(factors) - number == number
