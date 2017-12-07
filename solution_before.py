a2880fd26afbca2f770dbb0075677379a354ce58
Maxim Schepelin <m.schepelin@gmail.com>
2017-12-07 09:58:30 +0300

    Rewrite solution to the class cause it has an internal state

--- solution_before.py
+++ solution_before.py
@@ -1,11 +1,14 @@
 
-
-def is_factor(factor, number):
-    return number % factor == 0
-
-
 def is_perfect(number):
     pass
     # initialize factors
     # check if number a factor <--- simpliest thing to do
     # sum factors
+
+class PerfectNumberClassifier:
+
+    def __init__(self, number):
+        self.number = number
+
+    def is_factor(self, factor):
+        return self.number % factor == 0
--- tests_before.py
+++ tests_before.py
@@ -1,14 +1,20 @@
 import unittest
-from solution_before import (
-    is_factor,
-)
+from solution_before import PerfectNumberClassifier
 
 
 class TestIsPerfect(unittest.TestCase):
 
-    def test_is_factor_helper(self):
-        self.assertTrue(is_factor(1, 10))
-        self.assertFalse(is_factor(2, 3))
+    def test_is_factor__one_is_always_factor(self):
+        classifier = PerfectNumberClassifier(10)
+        self.assertTrue(classifier.is_factor(1))
+
+    def test_is_factor__multiple_numbers(self):
+        classifier = PerfectNumberClassifier(25)
+        self.assertTrue(classifier.is_factor(5))
+
+    def test_is_factor__negative_case(self):
+        classifier = PerfectNumberClassifier(7)
+        self.assertFalse(classifier.is_factor(3))
 
 
 if __name__ == '__main__':
